9bc63eb1edf345a82f2eaf34f785c0f1ba6d4e14
gksahoo2003-dev <gksahoo2003@gmail.com>
2026-03-28 12:48:04 +0530

    emoji + media + UI update

--- server.js
+++ server.js
@@ -6,17 +6,13 @@ const io = require("socket.io")(http);
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-    console.log("User connected");
 
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
+    socket.on("chat message", (data) => {
+        io.emit("chat message", data);
     });
 
-    socket.on("disconnect", () => {
-        console.log("User disconnected");
-    });
 });
 
 http.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+    console.log("Server running...");
 });
